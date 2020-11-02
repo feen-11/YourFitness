6c089f47a41cfded99a0a3e203a4f78f42f79b5d
Taichi Shiozawa <61722629+feen-11@users.noreply.github.com>
2020-11-02 18:24:37 +0900

    Add files via upload

---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-main',
+    'version' => 'dev-main',
+    'aliases' => 
+    array (
+    ),
+    'reference' => 'd52048c81265cd436967bb1e0c46b95e59daeaa6',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-main',
+      'version' => 'dev-main',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'd52048c81265cd436967bb1e0c46b95e59daeaa6',
+    ),
+  ),
+);
