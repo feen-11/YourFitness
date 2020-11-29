affab120ffae335e4e222890d6360c8b90d9b503
Taichi Shiozawa <61722629+feen-11@users.noreply.github.com>
2020-11-29 16:36:11 +0900

    Update easyLogin.php

--- easyLogin.php
+++ easyLogin.php
@@ -2,12 +2,16 @@
 
 require_once(__DIR__ . '/config.php');
 require_once(__DIR__ . '/User.php');
+require_once(__DIR__ . '/Post.php');
 
   $app = new User();
   $user = $app->easyLogin();
   $_SESSION['me'] = $user;
   $model = new Model();
   $model->postLoginDate();
+  $sample = new Post();
+  $sample->postFoodSample();
+  $sample->postTrainingSample();
   // redirect
   header('Location: ' . SITE_URL . '/index.php');
 
