bd92d25f3f030611c1f7d4562308d9d23f08dd4c
Taichi Shiozawa <61722629+feen-11@users.noreply.github.com>
2020-12-16 20:00:18 +0900

    Update ShowTraining.php

--- ShowTraining.php
+++ ShowTraining.php
@@ -1,14 +1,14 @@
 <?php
 
 require_once(__DIR__ . '/Controller.php');
-require_once(__DIR__ . '/../Model/User.php');
-require_once(__DIR__ . '/../Model/Post.php');
-require_once(__DIR__ . '/../Exception/InvalidToken.php');
-require_once(__DIR__ . '/../Exception/InvalidTimeflame.php');
-require_once(__DIR__ . '/../Exception/InvalidFood.php');
-require_once(__DIR__ . '/../Exception/InvalidIntakeCalorie.php');
-require_once(__DIR__ . '/../Exception/InvalidTraining.php');
-require_once(__DIR__ . '/../Exception/InvalidBurnCalorie.php');
+require_once(__DIR__ . '/User.php');
+require_once(__DIR__ . '/Post.php');
+require_once(__DIR__ . '/InvalidToken.php');
+require_once(__DIR__ . '/InvalidTimeflame.php');
+require_once(__DIR__ . '/InvalidFood.php');
+require_once(__DIR__ . '/InvalidIntakeCalorie.php');
+require_once(__DIR__ . '/InvalidTraining.php');
+require_once(__DIR__ . '/InvalidBurnCalorie.php');
 
 class ShowTraining extends Controller {
   
