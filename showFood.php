827293b16cf57b52df896d105760482f2ee9ceba
Taichi Shiozawa <61722629+feen-11@users.noreply.github.com>
2020-12-16 20:01:10 +0900

    Update showFood.php

--- showFood.php
+++ showFood.php
@@ -1,7 +1,7 @@
 <?php
 
-require_once(__DIR__ . '/../lib/Controller/ShowFood.php');
-require_once(__DIR__ . '/../config/config.php');
+require_once(__DIR__ . '/ShowFood.php');
+require_once(__DIR__ . '/config.php');
 
 $app = new ShowFood();
 $app->run();
